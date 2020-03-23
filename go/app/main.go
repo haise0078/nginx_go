f0d5e570d8d07d6dd197b69cc3f0dafea33c8549
RikuTatsumi <gffeva@gmail.com>
2020-03-24 08:06:25 +0900

    modify Counter structure

--- go/app/main.go
+++ go/app/main.go
@@ -9,29 +9,26 @@ import (
 )
 
 type Counter struct {
-	Id    int
 	Count int
 	Time  time.Time
 }
 
-var CounterById map[int]*Counter
+var counter Counter
 
 func main() {
-	var counter Counter
-	CounterById = make(map[int]*Counter)
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if CounterById[1] == nil {
-			counter = Counter{Id: 1, Count: 1, Time: time.Now()}
-			CounterById[counter.Id] = &counter
+		if counter.Count == 0 {
+			counter = Counter{Count: 1, Time: time.Now()}
+
 		} else {
-			log.Printf("%v", CounterById[1].Count)
-			CounterById[1].Count = CounterById[1].Count + 1
-			CounterById[1].Time = time.Now()
+			log.Printf("%v", counter.Count)
+			counter.Count = counter.Count + 1
+			counter.Time = time.Now()
 		}
 		log.Print("Accepted")
 		message := r.URL.Path
-		message = "This is " + strconv.Itoa(CounterById[1].Count) + "'s Count!\n"
-		message += "Recent Request is " + CounterById[1].Time.String()
+		message = "This is " + strconv.Itoa(counter.Count) + "'s Count!\n"
+		message += "Recent Request is " + counter.Time.String()
 		t, _ := template.ParseFiles("template/index.html")
 		t.Execute(w, message)
 	})
