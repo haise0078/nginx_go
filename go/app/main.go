5145dc71d72d7e7ed1fa669f14a935fc4e141790
RikuTatsumi <gffeva@gmail.com>
2020-04-05 15:12:21 +0900

    適当に作った諸々を直した

--- go/app/main.go
+++ go/app/main.go
@@ -19,21 +19,8 @@ func main() {
 	mux := http.NewServeMux()
 	files := http.FileServer(http.Dir("public"))
 	mux.Handle("/static/", http.StripPrefix("/static/", files))
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if counter.Count == 0 {
-			counter = Counter{Count: 1, Time: time.Now()}
-		} else {
-			log.Printf("%v", counter.Count)
-			counter.Count = counter.Count + 1
-			counter.Time = time.Now()
-		}
-		log.Print("Accepted")
-		message := r.URL.Path
-		message = "This is " + strconv.Itoa(counter.Count) + "'s Count!\n"
-		message += "Recent Request is " + counter.Time.String()
-		t, _ := template.ParseFiles("template/index.html")
-		t.Execute(w, message)
-	})
+	mux.HandleFunc("/", index)
+	mux.HandleFunc("/imageTest", imageTest)
 	log.Print("starting web server")
 	server := &http.Server{
 		Addr:    ":3000",
@@ -42,4 +29,28 @@ func main() {
 	if err := server.ListenAndServe(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	if counter.Count == 0 {
+		counter = Counter{Count: 1, Time: time.Now()}
+	} else {
+		log.Printf("%v", counter.Count)
+		counter.Count = counter.Count + 1
+		counter.Time = time.Now()
+	}
+	log.Print("Accepted")
+	message := r.URL.Path
+	message = "This is " + strconv.Itoa(counter.Count) + "'s Count!\n"
+	message += "Recent Request is " + counter.Time.String()
+	t, _ := template.ParseFiles("template/index.html")
+	t.Execute(w, message)
+}
+
+func imageTest(w http.ResponseWriter, r *http.Request) {
+	log.Print("Accepted")
+	message := r.URL.Path
+	message = "This is Image Test."
+	t, _ := template.ParseFiles("template/imageTest.html")
+	t.Execute(w, message)
 }
